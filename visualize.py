bcca3709ef72ef8c4d11aedf63a470067ed984e9
vreddy <vreddy@sequoia.cm.cluster>
2018-08-03 16:40:29 +0200

    second

--- visualize.py
+++ visualize.py
@@ -1,2 +1,15 @@
-import sklearn as skl
+
+label_file = './action_labels.txt'
+action_labels = {}
+
+# read label files and create a dictionary 
+with open(label_file, 'r') as f:
+    for l in f:
+	line = l.strip().split(',')
+	#remove empty labels
+	line = [x for x in line if x]
+	action_labels[line[0]] = line[1:]
+	
+print action_labels
+
 
